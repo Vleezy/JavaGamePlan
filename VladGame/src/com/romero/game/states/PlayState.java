5dd30b3d8731a1ec80bfca02a984ba6ae5b536bf
Vleezy <vladdromero@gmail.com>
2021-02-17 22:47:36 -0500

    Added KeyHandler

--- VladGame/src/com/romero/game/states/PlayState.java
+++ VladGame/src/com/romero/game/states/PlayState.java
@@ -4,6 +4,7 @@ import com.romero.game.util.KeyHandler;
 import com.romero.game.util.MouseHandler;
 
 import java.awt.Graphics2D;
+import java.awt.Color;
 
 public class PlayState extends GameState {
 
@@ -16,7 +17,9 @@ public class PlayState extends GameState {
     }
 
     public void input(MouseHandler mouse, KeyHandler key){
-
+        if(key.up.down){
+            System.out.println("'W' button was pressed");
+        }
     }
 
     public void render(Graphics2D g){
